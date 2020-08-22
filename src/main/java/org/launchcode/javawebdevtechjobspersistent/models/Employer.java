104dcb668c39367a13f976be009d3fb20f049ab6
Denis Duarte <duarte.denis11@gmail.com>
2020-08-22 15:04:41 -0400

    Part 1 and Part 2 Complete.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "This field cannot be blank.")
+    @Size(max = 100, message = "Location name is too long.")
+    private String location;
 
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,19 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Skill extends AbstractEntity {
+
+    @Size(max = 500, message = "Description is too long.")
+    private String description;
 
 }
